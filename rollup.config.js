9797612584117de166bb34df57f476db8c442fe5
Tony Quetano <tony.quetano@planttheidea.com>
2022-08-13 12:25:27 -0400

    Issue 79 - incorrect map relative reference (#82)
    * update dependencies to latest
    
    * improve rollup build setup
    
    * remove `dist-prev` used for debugging sourcemaps
    
    * revert debugging changes
    
    * update CHANGELOG

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-import tsc from 'typescript';
+import localTypescript from 'typescript';
 
 import pkg from './package.json';
 
@@ -12,29 +12,39 @@ const UMD_CONFIG = {
     format: 'umd',
     name: pkg.name,
     sourcemap: true,
+    sourcemapPathTransform(sourcePath) {
+      const [, sourceFile] = sourcePath.split('/src/');
+
+      return `../src/${sourceFile}`;
+    },
   },
-  plugins: [typescript({ typescript: tsc })],
+  plugins: [typescript({ typescript: localTypescript })],
 };
 
-const FORMATTED_CONFIG = Object.assign({}, UMD_CONFIG, {
+const FORMATTED_CONFIG = {
+  ...UMD_CONFIG,
   output: [
-    Object.assign({}, UMD_CONFIG.output, {
+    {
+      ...UMD_CONFIG.output,
       file: pkg.main,
       format: 'cjs',
-    }),
-    Object.assign({}, UMD_CONFIG.output, {
+    },
+    {
+      ...UMD_CONFIG.output,
       file: pkg.module,
       format: 'es',
-    }),
+    },
   ],
-});
+};
 
-const MINIFIED_CONFIG = Object.assign({}, UMD_CONFIG, {
-  output: Object.assign({}, UMD_CONFIG.output, {
+const MINIFIED_CONFIG = {
+  ...UMD_CONFIG,
+  output: {
+    ...UMD_CONFIG.output,
     file: pkg.browser.replace('.js', '.min.js'),
     sourcemap: false,
-  }),
-  plugins: UMD_CONFIG.plugins.concat([terser()]),
-});
+  },
+  plugins: [...UMD_CONFIG.plugins, terser()],
+};
 
 export default [UMD_CONFIG, FORMATTED_CONFIG, MINIFIED_CONFIG];
