87c2f6b490e84253713f772190961f1511112db6
Tony Quetano <tony.quetano@planttheidea.com>
2025-11-11 18:15:21 +0100

    migrate tests for `isKeyEqual` and `isKeyItemEqual` option

---
+++ src/__tests__/isKeyItemEqual.ts
@@ -0,0 +1,79 @@
+import { describe, expect, test, vi } from 'vitest';
+import { memoize } from '../index.js';
+
+describe('deep', () => {
+  const method = ({ one, two }: { one: number; two: { deep: number } }) => [
+    one,
+    two.deep,
+  ];
+
+  test('memoizes based on the deep values via option', () => {
+    const fn = vi.fn(method);
+    const memoized = memoize(fn, { isKeyItemEqual: 'deep' });
+
+    const resultA = memoized({ one: 1, two: { deep: 2 } });
+    const resultB = memoized({ one: 1, two: { deep: 2 } });
+
+    expect(resultA).toEqual([1, 2]);
+    expect(resultA).toBe(resultB);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('shallow', () => {
+  const method = ({ one, two }: { one: number; two: { deep: number } }) => [
+    one,
+    two.deep,
+  ];
+
+  test('memoizes based on the shallow values via option', () => {
+    const fn = vi.fn(method);
+    const memoized = memoize(fn, { isKeyItemEqual: 'shallow' });
+
+    const two = { deep: 2 };
+
+    const resultA = memoized({ one: 1, two });
+    const resultB = memoized({ one: 1, two });
+
+    expect(resultA).toEqual([1, 2]);
+    expect(resultA).toBe(resultB);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+
+    const resultC = memoized({ one: 1, two: { ...two } });
+
+    expect(resultC).toEqual(resultA);
+    expect(resultC).not.toBe(resultA);
+
+    expect(fn).toHaveBeenCalledTimes(2);
+  });
+});
+
+describe('custom', () => {
+  test('memoizes based on the custom comparator via option', () => {
+    const method = (one: number, two: { deep: number }) => [one, two.deep];
+    const fn = vi.fn(method);
+    const memoized = memoize(fn, {
+      isKeyItemEqual: (prevArg, nextArg) => {
+        if (typeof prevArg === 'number') {
+          return prevArg === nextArg;
+        }
+
+        if (typeof prevArg === 'object') {
+          return prevArg.deep === nextArg.deep;
+        }
+
+        return false;
+      },
+    });
+
+    const resultA = memoized(1, { deep: 2 });
+    const resultB = memoized(1, { deep: 2 });
+
+    expect(resultA).toEqual([1, 2]);
+    expect(resultA).toBe(resultB);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+});
