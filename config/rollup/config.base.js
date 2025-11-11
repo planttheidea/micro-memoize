d11d8ad4278d4b6234e4bb4a9cb48ea29ba6f183
Tony Quetano <tony.quetano@planttheidea.com>
2025-11-11 20:46:57 +0100

    remove unnecessary `@rollup/plugin-node-resolve`

--- config/rollup/config.base.js
+++ config/rollup/config.base.js
@@ -1,8 +1,7 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { fileURLToPath } from "url";
-import pkg from "./packageJson.js";
+import { fileURLToPath } from 'url';
+import pkg from './packageJson.js';
 
-export const ROOT = fileURLToPath(new URL("..", import.meta.url));
+export const ROOT = fileURLToPath(new URL('..', import.meta.url));
 
 const external = [
   ...Object.keys(pkg.dependencies || {}),
@@ -16,16 +15,12 @@ const globals = external.reduce((globals, name) => {
 
 export default {
   external,
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
-    exports: "named",
+    exports: 'named',
     globals,
     name: pkg.name,
     sourcemap: true,
   },
-  plugins: [
-    nodeResolve({
-      mainFields: ["module", "browser", "main"],
-    }),
-  ],
+  plugins: [],
 };
