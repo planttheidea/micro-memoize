6b4e2981a22570b27ca32124cbcd7786c0ffcfdd
Tony Quetano <tony.quetano@planttheidea.com>
2026-09-01 10:54:20 +0200

    rerun init

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,6 +1,9 @@
 import { createRollupConfig } from '@planttheidea/build-tools';
 
 export default createRollupConfig({
+  cjs: true,
+  config: 'config',
+  source: 'src',
   sourceMap: true,
   umd: true,
 });
